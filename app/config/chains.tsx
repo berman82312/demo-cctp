abcba653b325f4a589b6647ae46bc97c3c229d7b
Littlefish <berman82312@gmail.com>
2024-10-01 14:20:28 +0800

    Feat: chain config add chain id

--- app/config/chains.tsx
+++ app/config/chains.tsx
@@ -1,8 +1,11 @@
+import { baseSepolia, sepolia, type Chain } from "viem/chains";
+
 type PublicPath = string;
 
 type ChainConfig = {
     id: string,
     name: string,
+    chainId: Chain['id'],
     cctpId: number,
     icon: PublicPath,
     isTestnet: boolean
@@ -11,6 +14,7 @@ type ChainConfig = {
 export const Sepolia: ChainConfig = {
     id: 'sepolia',
     name: 'Sepolia',
+    chainId: sepolia.id,
     cctpId: 0,
     icon: '/icons/sepolia.svg',
     isTestnet: true
@@ -19,6 +23,7 @@ export const Sepolia: ChainConfig = {
 export const BaseSepolia: ChainConfig = {
     id: 'base_sepolia',
     name: 'Base Sepolia',
+    chainId: baseSepolia.id,
     cctpId: 6,
     icon: '/icons/base-sepolia.svg',
     isTestnet: true
