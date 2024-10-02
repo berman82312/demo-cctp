3e6b0fccc8c8a768a52d9f113e4fbf95067a8e58
Littlefish <berman82312@gmail.com>
2024-10-02 09:05:12 +0800

    Style: formatting code

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ConnectButton/>
+        <ConnectButton />
         <h2 className="text-2xl">Transfer your USDC</h2>
-        <SourceForm/>
+        <SourceForm />
         <Divider className="w-full">To</Divider>
-        <DestinationForm/>
+        <DestinationForm />
       </main>
     </div>
   );
